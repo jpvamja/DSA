27f612a2cad341569737538ee83122010c758431
jpvamja <jpvamja@gmail.com>
2025-12-18 18:24:44 +0530

    Add Q13.java to implement a function that finds the length of the longest sub-array that sums to a given integer k

---
+++ 03.Solve_Problems_on_Arrays/Easy/Q13.java
@@ -0,0 +1,42 @@
+// Given an array nums of size n and an integer k, find the length of the longest sub-array that sums to k. If no such sub-array exists, return 0.
+
+public class Q13 {
+    public int longestSubarray(int[] nums, int k) {
+        int n = nums.length;
+        int maxLen = 0;
+
+        int left = 0, right = 0;
+
+        int sum = nums[0];
+
+        while (right < n) {
+
+            while (left <= right && sum > k) {
+                sum -= nums[left];
+                left++;
+            }
+
+            if (sum == k) {
+                maxLen = Math.max(maxLen, right - left + 1);
+            }
+
+            right++;
+            if (right < n) {
+                sum += nums[right];
+            }
+        }
+
+        return maxLen;
+    }
+
+    public static void main(String[] args) {
+
+        int[] arr = { 10, 5, 2, 7, 1, 9 };
+        int k = 15;
+
+        Q13 sol = new Q13();
+        int ans = sol.longestSubarray(arr, k);
+
+        System.out.println(ans);
+    }
+}
